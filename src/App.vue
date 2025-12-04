e4460a8ed97db3795fa04c3bdcfd9b4fcd562ca1
Dabert Sébastien <sebdab86@gmail.com>
2025-12-04 20:52:41 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+</style>
